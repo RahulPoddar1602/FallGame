75f24b19aaca50eaa75f7118924138f4eda9f387
RahulPoddar1602 <rahul1999p@gmail.com>
2023-01-02 09:03:37 +0530

    difficulty increased

--- script.js
+++ script.js
@@ -4,34 +4,50 @@ var interval;
 var both = 0;
 var counter = 0;
 var currentBlocks = [];
-
+var leftcounter = Math.pow(1.03,counter) 
+var runner =leftcounter;
+var topcounter = 0.8*Math.pow(1.01,counter)
 function moveLeft(){
-    var left = parseInt(window.getComputedStyle(character).getPropertyValue("left"));
-    if(left>0){
-        if(Math.pow(1.02,counter)<1.9)
-        character.style.left = left - Math.pow(1.02,counter) + "px";
-        else
-        character.style.left = left - 2 + "px";
-    }
+    var left = parseFloat(window.getComputedStyle(character).getPropertyValue("left"));
+    if(left>0)
+    character.style.left = left - 1.5*runner + "px";
 }
+// function moveFastLeft(){
+//     var left = parseFloat(window.getComputedStyle(character).getPropertyValue("left"));
+//     if(left>0)
+//     character.style.left = left - 1.5*runner + "px";
+// }
 function moveRight(){
-    var left = parseInt(window.getComputedStyle(character).getPropertyValue("left"));
-    if(left<380){
-        if(Math.pow(1.02,counter)<1.9)
-        character.style.left = left + Math.pow(1.02,counter) + "px";
-        else
-        character.style.left = left + 2 + "px";
-    }
+    var left = parseFloat(window.getComputedStyle(character).getPropertyValue("left"));
+    if(left<380)
+    character.style.left = left + 1.5*runner + "px";
 }
+// function moveFastRight(){
+//     var left = parseFloat(window.getComputedStyle(character).getPropertyValue("left"));
+//     if(left<380)
+//     character.style.left = left + 1.5*runner + "px";
+// }   
+// document.addEventListener("keydown", event => {
+//     if(event.key==="Shift")
+//     runner=1.5*leftcounter;
+//     else
+//     runner=leftcounter;
+// })
 document.addEventListener("keydown", event => {
     if(both==0){
         both++;
-        if(event.key==="ArrowLeft"){
+        if(event.key==="ArrowLeft" ){
             interval = setInterval(moveLeft, 1);
         }
+        // if(event.key==="a" ){
+        //     interval = setInterval(moveFastLeft, 1);
+        // }
         if(event.key==="ArrowRight"){
             interval = setInterval(moveRight, 1);
         }
+        // if(event.key==="d"){
+        //     interval = setInterval(moveFastRight, 1);
+        // }
     }
 });
 document.addEventListener("keyup", event => {
@@ -65,7 +81,7 @@ var blocks = setInterval(function(){
     var characterTop = parseInt(window.getComputedStyle(character).getPropertyValue("top"));
     var characterLeft = parseInt(window.getComputedStyle(character).getPropertyValue("left"));
     var drop = 0;
-    if(characterTop <= 0){
+    if(characterTop <= 0 ){
         alert("Game over. Score: "+(counter-9));
         clearInterval(blocks);
         location.reload();
@@ -76,8 +92,18 @@ var blocks = setInterval(function(){
         let ihole = document.getElementById("hole"+current);
         let iblockTop = parseFloat(window.getComputedStyle(iblock).getPropertyValue("top"));
         let iholeLeft = parseFloat(window.getComputedStyle(ihole).getPropertyValue("left"));
-        iblock.style.top = iblockTop - 0.5 + "px";
-        ihole.style.top = iblockTop - 0.5 + "px";
+        // if(0.2*Math.pow(1.1,counter)<1)
+        // {
+        //     iblock.style.top = iblockTop - 0.2*Math.pow(1.1,counter) + "px";
+        //     ihole.style.top = iblockTop - 0.2*Math.pow(1.1,counter)  + "px";
+        // }
+        // else
+        // {
+        //     iblock.style.top = iblockTop - 1 + "px";
+        //     ihole.style.top = iblockTop - 1  + "px";
+        // }
+        iblock.style.top = iblockTop - topcounter + "px";
+        ihole.style.top = iblockTop - topcounter  + "px";
         if(iblockTop < -20){
             currentBlocks.shift();
             iblock.remove();
@@ -85,16 +111,16 @@ var blocks = setInterval(function(){
         }
         if(iblockTop-20<characterTop && iblockTop>characterTop){
             drop++;
-            if(iholeLeft<=characterLeft && iholeLeft+20>=characterLeft){
+            if(iholeLeft<=characterLeft && iholeLeft+80>=characterLeft){
                 drop = 0;
             }
         }
     }
     if(drop==0){
         if(characterTop < 480){
-            character.style.top = characterTop + 2 + "px";
+            character.style.top = characterTop + leftcounter + "px";
         }
     }else{
-        character.style.top = characterTop - 0.5 + "px";
+        character.style.top = characterTop - topcounter + "px";
     }
 },1);
